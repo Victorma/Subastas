da3a05ca80cfeb4bb4ad8da478d0cf525e28ecb4
VictormaFire <victorma-fire@hotmail.com>
2014-05-21 02:11:51 +0200

    Items
    Items añadidos, creables desde assets->create->item

---
+++ Assets/Editor/ItemEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ItemEditor : MonoBehaviour {
+
+[MenuItem("Assets/Create/Item")]
+	public static void createIsoTextureAsset(){
+		Item asset = new Item();  //scriptable object
+		ProjectWindowUtil.CreateAsset(asset, "Item.asset");
+		//AssetDatabase.CreateAsset(asset, "MyAsset.asset");
+		//EditorUtility.FocusProjectWindow();
+		Selection.activeObject = asset;    
+	}
+}
---
+++ Assets/Item.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Item : ScriptableObject{
+
+	public enum RareType{
+		Common, Uncommon, Rare, Epic, Legendary
+	}
+
+	public string name;
+	public Sprite image;
+	public RareType rareness;
+	public float buyingPrice;
+	public float sellingPrice;
+	public List<string> types;
+
+}
